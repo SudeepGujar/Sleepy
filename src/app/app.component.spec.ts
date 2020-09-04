14a1a5d1e3542be4522a67c5f0570ee1de2d5ba3
Sudeep <sudeepsgujar@gmail.com>
2020-09-04 23:43:01 +0530

    Changed app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-dream-app'`, () => {
+  it(`should have as title 'sleepy-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-dream-app');
+    expect(app.title).toEqual('sleepy-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-dream-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('sleepy-app app is running!');
   });
 });
